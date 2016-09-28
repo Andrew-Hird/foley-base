67a75484ce146895ff838f9877f05c1113b8d385
Andrew Hird <andrew.james.hird@gmail.com>
2016-09-29 11:21:31 +1300

    audio showing

--- client/components/RecList.jsx
+++ client/components/RecList.jsx
@@ -4,7 +4,9 @@ export default React.createClass({
   render() {
     return (
       <div>
-        {this.props.audio}
+        <h4>{this.props.audio}</h4>
+        <audio controls="controls" src ={'https://s3-us-west-2.amazonaws.com/audio-foley-base/' + this.props.audio} type="audio/ogg"></audio>
+
       </div>
     )
   }
